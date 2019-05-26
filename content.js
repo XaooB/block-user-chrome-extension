70308f7440f03bfb978443230759d9ad17b57e22
XaooB <prawo123@interia.pl>
2019-05-26 20:47:11 +0200

    mada function to avoid multiple code repeats

--- content.js
+++ content.js
@@ -1,21 +1,24 @@
 (function() {
+  function hideComments(comments, users) {
+    for (let i = 0; i < comments.length; i++) {
+      for (let j = 0; j < users.length; j++) {
+        if(comments[i].children[0].innerHTML.toLowerCase().indexOf(users[j].trim()) !== -1)
+        comments[i].style.display = 'none';
+      }
+    }
+  }
+
   const commentsNodes = document.querySelectorAll('#komentarze dl');
   let blockedUsers = localStorage.getItem('blockedUsers');
 
   if(blockedUsers === null)
     localStorage.setItem('blockedUsers', '');
 
-    blockedUsers = localStorage.getItem('blockedUsers');
+  blockedUsers = localStorage.getItem('blockedUsers');
 
   if(blockedUsers.length) {
     let blockedUsersArray = blockedUsers.split(',');
-    //hide comments
-    for (let i = 0; i < commentsNodes.length; i++) {
-      for (let j = 0; j < blockedUsersArray.length; j++) {
-        if(commentsNodes[i].children[0].innerHTML.toLowerCase().indexOf(blockedUsersArray[j].trim()) !== -1)
-        commentsNodes[i].style.display = 'none';
-      }
-    }
+    hideComments(commentsNodes, blockedUsersArray);
   }
 
 
@@ -23,22 +26,14 @@
     const commentsNodes = document.querySelectorAll('#komentarze dl');
 
     localStorage.setItem('blockedUsers', request);
-
-    //wali błędem, bo to jest puste
-    const blockedUsersArray = localStorage.getItem('blockedUsers').split(',');
+    let blockedUsersArray = localStorage.getItem('blockedUsers').split(',');
 
     //otherwise send list back
     sendResponse({error: false, usersList: blockedUsersArray})
 
     //if theres no users to block blockedUsersArray[0] gonna be "".
-    if(blockedUsersArray[0].length > 1) {
-    //hide comments
-      for (let i = 0; i < commentsNodes.length; i++) {
-        for (let j = 0; j < blockedUsersArray.length; j++) {
-          if(commentsNodes[i].children[0].innerHTML.toLowerCase().indexOf(blockedUsersArray[j].trim()) !== -1)
-            commentsNodes[i].style.display = 'none';
-        }
-      }
-    }
+    if(blockedUsersArray[0].length > 1)
+      hideComments(commentsNodes, blockedUsersArray);
+
   });
 }());
