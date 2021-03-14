70e7395bca006a0b0b231cbbfac875a3530f7a72
Bartas <prawo123@interia.pl>
2021-03-14 20:36:13 +0100

    [WIP] Refactoring extension to make it work on RM 2.

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
+//Event listener for injected.js
+chrome.runtime.onMessageExternal.addListener(function (message, sender) {
+    if (message.interception) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {interception: true});
+        });
+        return true;
+    }
+    return false;
+});
+
 chrome.runtime.onMessage.addListener(function (message, sender) {
     chrome.browserAction.setBadgeBackgroundColor({color: [238, 50, 78, 255]});
     chrome.browserAction.setBadgeText({text: message.data});
--- content.js
+++ content.js
@@ -1,29 +1,68 @@
-var id = window.setInterval(function () {
-    const _comments_holder = '.comments-list .user-comment';
-    const commentsNodes = document.querySelectorAll(_comments_holder);
-    let blockedUsers = getBlockedUsers();
-
-    //Due to content being loaded ansynchronously we need to check whenever comments has been loaded or not
-    //Then we load the content script to make changes to the loaded page
-
-    if (commentsNodes.length) {
-        window.clearInterval(id);
-
-        if (blockedUsers !== null) {
-            hideComments(commentsNodes, blockedUsers);
-        } else {
-            saveBlockedUsers([]);
-        }
+const config = {
+    messages: {
+        userMarkedAsBlocked: 'Użytkownik został dodany do listy.',
+        userBlocked: 'Użytkownik znajduje się na liście zablokowanych.'
+    },
+    userType: {
+        blocked: 'BLOCKED',
+        unblocked: 'UNBLOCKED',
+    },
+    labels: {
+        block: 'Zablokuj',
+        unblock: 'Odblokuj',
+        showBlockedText: 'Pokaż komentarz'
+    },
+    selectors: {
+        userName: '.user-comment__name',
+        commentHolder: '.comments-list .user-comment',
+        commentText: '.comment-text',
+        commentAction: '.comments-action',
+        commentsBlocked: `[data-user-type="BLOCKED"]`
     }
-}, 1000);
+}
+
+//init app
+initApp();
+
+function initApp() {
+    var script = document.createElement('script');
+    script.src = chrome.extension.getURL('injected.js');
+    script.onload = function() {
+        this.remove();
+    };
+    
+    (document.head || document.documentElement).appendChild(script);
+    
+    chrome.runtime.onMessage.addListener(function (message, sender) {
+        let commentsNodes = document.querySelectorAll(config.selectors.commentHolder);
+        let blockedUsers = getBlockedUsers();
+
+        if (message.interception) {
+            hideComments(commentsNodes, blockedUsers);
+        }
+
+        if(message.reset) {
+            localStorage.setItem('blockedUsers', '')
+            let blockedComments = document.querySelectorAll(config.selectors.commentsBlocked);
+            let blockedCommentsHolder = Array.from(blockedComments).map(function(el) {
+               return el.closest(config.selectors.commentHolder) 
+            });
+            let blockedUsers = getBlockedUsers();
+            
+            //probably needs to be rewritten to be able to show all comments on reset
+            //we need to store reference for old comment to instead of mutating original comment we need to hide it
+            //and add custom div with information that particular comment is blocked
+            hideComments(blockedCommentsHolder, blockedUsers);
+        }
+    });
+}
 
 function hideComments(comments, users, deletedComments = 0) {
-    const _comments_blocked_text = 'Użytkownik zablokowany. Aby zobaczyc zawartosc musisz go odblokowac.';
-
+    const _comments_blocked_text = config.messages.userBlocked;
     for (let i = 0; i < comments.length; i++) {
-        let commentUser = comments[i].querySelector('.user-comment__name').textContent.trim();
+        let commentUser = comments[i].querySelector(config.selectors.userName).textContent.trim();
         if (users.includes(commentUser)) {
-            let comment = comments[i].querySelector('.comment-text'),
+            let comment = comments[i].querySelector(config.selectors.commentText),
                 oldComment = comment.textContent.trim();
 
             comment.setAttribute(
@@ -31,41 +70,51 @@ function hideComments(comments, users, deletedComments = 0) {
                 "color: #ff0033; font-style: italic; font-size: 13px; padding: 4px 0;"
             );
             comment.textContent = _comments_blocked_text;
-            createActionLink('Odblokuj', comments[i], oldComment);
+            comment.dataset.userType = config.userType.blocked;
+            createActionLink(config.labels.unblock, comments[i], oldComment);
             deletedComments++;
         } else {
-            createActionLink('Zablokuj', comments[i]);
+            createActionLink(config.labels.block, comments[i]);
         }
     }
     //send number of deleted comments to the background
     if (deletedComments !== 0) {
-        return chrome.runtime.sendMessage({data: `${deletedComments}`});
+        chrome.runtime.sendMessage({data: `${deletedComments}`});
+    } else {
+        chrome.runtime.sendMessage({data: ''});
     }
-
-    chrome.runtime.sendMessage({data: ''});
 }
 
 function createActionLink(nodeName, holder, oldComment = null) {
     let anchor = document.createElement('a'),
-        userName = holder.querySelector('.user-comment__name').textContent.trim();
+        userName = holder.querySelector(config.selectors.userName).textContent.trim(),
+        userType = holder.querySelector(config.selectors.commentText).dataset.userType;
 
     anchor.classList.add('unblock-user', 'link-naked', 'text-color_appblue', 'box-inline', 'shift-margin_2-right', 'user-actions');
     anchor.setAttribute('style', 'font-weight: bold;');
     anchor.textContent = nodeName;
-    anchor.title = nodeName + ' uzytkownika ' + userName;
+    anchor.title = nodeName + ' użytkownika ' + userName;
     anchor.dataset.userName = userName;
 
-    if (nodeName === 'Zablokuj') {
-        anchor.onclick = blockUser;
-    } else {
-        anchor.onclick = unblockUser;
-    }
+    switch (userType) {
+        case config.userType.blocked:
+            anchor.onclick = unblockUser;
+            break;
 
+        case config.userType.unblocked:
+            anchor.onclick = blockUser;
+            break;
+
+        default:
+            anchor.onclick = blockUser;
+            break;
+    }
+    
     if (oldComment) {
         anchor.dataset.hiddenContent = oldComment;
     }
 
-    holder.querySelector('.comments-action').append(anchor);
+    holder.querySelector(config.selectors.commentAction).append(anchor);
 }
 
 function unblockUser(event) {
@@ -80,9 +129,10 @@ function unblockUser(event) {
     saveBlockedUsers(blockedUsers.join());
 
     comment.textContent = this.dataset.hiddenContent;
+    comment.dataset.userType = config.userType.unblocked;
     comment.setAttribute('style', '');
     element.querySelector('.unblock-user').remove();
-    createActionLink('Zablokuj', element, oldComment);
+    createActionLink(config.labels.block, element, oldComment);
 }
 
 function blockUser(event) {
@@ -97,20 +147,21 @@ function blockUser(event) {
     saveBlockedUsers(newList);
 
     comment.textContent = 'Użytkownik zablokowany. Aby zobaczyc zawartosc musisz go odblokowac.';
+    comment.dataset.userType = config.userType.blocked;
     comment.setAttribute(
         "style",
         "color: #ff0033; font-style: italic; font-size: 13px; padding: 4px 0;"
     );
 
     element.querySelector('.unblock-user').remove();
-    createActionLink('Odblokuj', element, oldComment);
+    createActionLink(config.labels.unblock, element, oldComment);
 }
 
 function getBlockedUsers() {
-    return localStorage.getItem('blockedUsers').split(',') || [];
+    let userList = localStorage.getItem('blockedUsers');
+    return userList ? userList.split(',') : [];
 }
 
 function saveBlockedUsers(users) {
     localStorage.setItem('blockedUsers', users);
 }
-
